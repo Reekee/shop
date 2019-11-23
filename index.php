d369d54c32e3039425b6c526a690b1dfd0ffce65
Reekee <r_kee19384@hotmail.com>
2019-11-23 10:08:16 +0700

    save

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>เว็บไซด์ร้านค้าออนไลน์</title>
+	<style type="text/css">
+		[name="btn-search"] {
+			font-size: 100px;
+			background-color: #00ff55;
+			border-radius: 20px;
+			padding: 30px;
+			color: #ff0000;
+			margin-right: 30px;
+		}
+	</style>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	OK
+	<input type="button" name="btn-search" value="ค้นหา">
+	<input type="input" name="txt-product-name">
+	<button type="button" class="btn btn-success">Success</button>
+	<form>
+	  <div class="form-group">
+	    <label for="exampleInputEmail1">Email address</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+	    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+	  </div>
+	  <div class="form-group">
+	    <label for="exampleInputPassword1">Password</label>
+	    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	  </div>
+	  <div class="form-group form-check">
+	    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+	    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+	  </div>
+	  <button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+</body>
+</html>
