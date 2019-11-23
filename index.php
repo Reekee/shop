bcb2a444e462252aea46b62aeb4cf3b89049460e
Reekee <r_kee19384@hotmail.com>
2019-11-23 11:17:38 +0700

    save

--- index.php
+++ index.php
@@ -6,9 +6,24 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<style>
+		html {
+			position: relative;
+			min-height: 100%;
+		}
+		#footer {
+			position: absolute;
+			bottom: 0px;
+			width: 100%;
+			background-color: red;
+			color: #fff;
+			font-size: 20px;
+			padding: 20px;
+		}
+	</style>
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav id="header" class="navbar navbar-expand-lg navbar-light bg-light">
 		<a class="navbar-brand" href="#">ร้านค้าออนไลน์</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -21,5 +36,27 @@
 			</form>
 		</div>
 	</nav>
+	<div id="body">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="list-group">
+						<a href="#" class="list-group-item list-group-item-action active">
+							หน้าหลัก
+						</a>
+						<a href="#" class="list-group-item list-group-item-action">ข้อมูลสินค้า</a>
+						<a href="#" class="list-group-item list-group-item-action">รายชื่อลูกค้า</a>
+						<a href="#" class="list-group-item list-group-item-action">การสั่งซื้อ</a>
+					</div>
+				</div>
+				<div class="col-md-10">
+					One of three columns
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		Footer
+	</div>
 </body>
 </html>
