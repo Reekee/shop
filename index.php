b71e0947e2fc64eb3fbed7630c81dac775a100f1
Reekee <r_kee19384@hotmail.com>
2019-11-23 10:50:55 +0700

    save

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
-	<button type="button" class="btn btn-danger">Danger</button>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#">ร้านค้าออนไลน์</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto"></ul>
+			<form class="form-inline my-2 my-lg-0">
+				<div>สมชาย สมใจ</div>
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">ออกจากระบบ</button>
+			</form>
+		</div>
+	</nav>
 </body>
 </html>
