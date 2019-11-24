631e690d3d79270e8c3d4d1869d3fca2ee691415
Reekee <r_kee19384@hotmail.com>
2019-11-24 09:22:24 +0700

    save

--- pages/product-add.php
+++ pages/product-add.php
@@ -1,1 +1,17 @@
-เพิ่มสินค้า
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="product_name">ชื่อสินค้า</label>
+      <input type="text" class="form-control" id="product_name" name="product_name" placeholder="ระบุชื่อสินค้า">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="price">ราคาสินค้า</label>
+      <input type="text" class="form-control" id="price" name="price" placeholder="ระบุราคาสินค้า">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="img">เลือกรูปสินค้า</label>
+    <input type="file" class="form-control-file" id="img" name="img">
+  </div>
+  <button type="submit" class="btn btn-success">บันทึกสินค้า</button>
+</form>
